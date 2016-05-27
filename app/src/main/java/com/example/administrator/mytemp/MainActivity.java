78341f8d5ede747a58ca79168629d40db9d7ecd2
xxyy110 <shawn_li@live.com>
2016-05-27 14:19:58 -0400

    Base check in

---
+++ app/src/main/java/com/example/administrator/mytemp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.administrator.mytemp;
+
+import android.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater actionBarMenuInflater = getMenuInflater();
+        actionBarMenuInflater.inflate(R.menu.actionbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
