881b8580ea9df925d78f7d7b785843f640703137
xxyy110 <shawn_li@live.com>
2016-05-27 16:04:06 -0400

    1.Complete Actionbar layout 2.Update Copyright info

--- app/src/main/java/com/example/administrator/mytemp/MainActivity.java
+++ app/src/main/java/com/example/administrator/mytemp/MainActivity.java
@@ -1,18 +1,23 @@
 /*
- * Copyright (c) $today.year.All Rights Reserved.
+ * Copyright (c) MyPrivateCo.
+ * Author:           Administrator
+ * Last Change : 27/05/16 4:03 PM
+ * All Rights Reserved.
  */
 
 package com.example.administrator.mytemp;
 
 
-
-import android.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 
+/**
+ * Entry point of this app
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -26,5 +31,20 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater actionBarMenuInflater = getMenuInflater();
         actionBarMenuInflater.inflate(R.menu.actionbar_menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_refresh:
+                return true;
+            case R.id.action_settings:
+                return true;
+            case R.id.action_about:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
