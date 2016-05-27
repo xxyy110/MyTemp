69cd0e7ecad5701fd06548b76cd5b996cda4aa75
xxyy110 <shawn_li@live.com>
2016-05-27 14:53:30 -0400

    Add copyright for testing

--- app/src/main/java/com/example/administrator/mytemp/MainActivity.java
+++ app/src/main/java/com/example/administrator/mytemp/MainActivity.java
@@ -1,10 +1,17 @@
+/*
+ * Copyright (c) $today.year.All Rights Reserved.
+ */
+
 package com.example.administrator.mytemp;
+
+
 
 import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+
 
 public class MainActivity extends AppCompatActivity {
 
