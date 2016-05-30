574b6007b84f32512e9a6a1ceab945d7bd547a91
xxyy110 <shawn_li@live.com>
2016-05-30 09:42:36 -0400

    Set up the layout for main activity

--- app/src/main/java/com/example/administrator/mytemp/MainActivity.java
+++ app/src/main/java/com/example/administrator/mytemp/MainActivity.java
@@ -1,28 +1,46 @@
 /*
  * Copyright (c) MyPrivateCo.
  * Author:           Administrator
- * Last Change : 27/05/16 4:03 PM
+ * Last Change : 10/06/16 3:45 PM
  * All Rights Reserved.
  */
 
 package com.example.administrator.mytemp;
 
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
  * Entry point of this app
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
+
+    private SensorManager sensorManager;
+    private Sensor tempSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        sensorManager = (SensorManager)this.getSystemService(this.SENSOR_SERVICE);
+        tempSensor = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+        if (tempSensor != null) {
+            sensorManager.registerListener(tempSensorListener, tempSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        else {
+            Toast.makeText(this, "Temperature Sensor Not Found", Toast.LENGTH_LONG).show();
+        }
         setContentView(R.layout.activity_main);
     }
 
@@ -46,5 +64,22 @@ public class MainActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
 
+    }
+
+    private SensorEventListener tempSensorListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            float sensorTemp = event.values[0];
+            DisplayTemperature((int)sensorTemp);
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
+
+    private void DisplayTemperature(int tempVal){
+        ((TextView)this.findViewById(R.id.tempValue)).setText(tempVal);
     }
 }
