5b25bb06704df455eca9c9754056a5087790c820
xxyy110 <shawn_li@live.com>
2016-05-30 09:42:36 -0400

    Set up the layout for main activity

--- app/src/test/java/com/example/administrator/mytemp/ExampleUnitTest.java
+++ app/src/test/java/com/example/administrator/mytemp/ExampleUnitTest.java
@@ -1,8 +1,15 @@
+/*
+ * Copyright (c) MyPrivateCo.
+ * Author:           Administrator
+ * Last Change : 27/05/16 4:03 PM
+ * All Rights Reserved.
+ */
+
 package com.example.administrator.mytemp;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
